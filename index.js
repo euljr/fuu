5ff6cf8bb44c55e35349b477b6a6408a6b215953
Euler Menezes Junior <euler@alunos.utfpr.edu.br>
2016-07-01 11:57:52 -0300

    First version

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 var errors = [];
 
 var fuu = {
-    /**
-     * Add new custom errors to array
-     *
-     * @param  {String} html
-     * @return {String}
-     */
     add: function (error) {
         if (Array.isArray(error))
             error.forEach(function (e) {
-                add(e);
+                fuu.add(e);
             }, this);
-        else
-            errors.push(e);
+        else {
+            if (!error.hasOwnProperty('code')) {
+                throw new Error('Error must have a code.');
+            }
+            errors.push(error);
+        }
     },
     handlers: {
         expressProd: function (err, req, res, next) {
-            res.status(err.status || 500);
+            var e = fuu.parseError(err);
+            res.status(e.status);
             res.send({
-                message: err.message,
-                error: err
+                message: e.message,
+                error: {}
             });
         },
         expressDev: function (err, req, res, next) {
-            res.status(err.status || 500);
+            var e = fuu.parseError(err);
+            res.status(e.status);
             res.send({
-                message: err.message,
-                error: err
+                message: e.message,
+                error: e.error
             });
         }
     },
     list: function () {
         console.log(errors);
+    },
+    get: function (code) {
+        return errors.filter(e => e.code == code)[0];
+    },
+    parseError: function (err) {
+        var e = undefined;
+        var message;
+        var error;
+        var status;
+        if (err.hasOwnProperty('code'))
+            e = fuu.get(err.code);
+        if (typeof e !== undefined) {
+            message = e.message ? e.message : (err.error ? (err.error.message || '') : '');
+            status = e.status ? e.status : (err.error ? (err.error.status || 500) : 500);
+            error = err.error || {};
+        } else {
+            message = err.message;
+            status = err.status || 500;
+            error = err.error;
+        }
+
+        return { message: message, error: error, status: status };
     }
 };
 
